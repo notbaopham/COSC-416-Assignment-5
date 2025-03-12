ccd243c03c6f2a92e423ff9c7779ec81c346589f
Toby Nguyen <ductoby2003@gmail.com>
2025-03-11 20:17:50 -0700

    Revert "[T] Finished Game Over UI and Lives mechanics"

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,25 +1,14 @@
-﻿using System.Collections;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
-    [SerializeField] private TextMeshProUGUI livesText;
-    [SerializeField] private GameObject gameOverUI;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
 
     private int currentBrickCount;
     private int totalBrickCount;
 
-    private void Start()
-    {
-        livesText.text = $"Lives: {maxLives}";
-
-        // hide game over UI
-        gameOverUI.SetActive(false);
-    } 
     private void OnEnable()
     {
         InputHandler.Instance.OnFire.AddListener(FireBall);
@@ -53,21 +42,6 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         maxLives--;
         // update lives on HUD here
         // game over UI if maxLives < 0, then exit to main menu after delay
-        livesText.text = $"Lives: {maxLives}";
-        if(maxLives <= 0)
-        {
-            gameOverUI.SetActive(true);
-            Time.timeScale = 0; // Freeze the game
-            StartCoroutine(ExitToMainMenuAfterDelay());
-        }
         ball.ResetBall();
-    }
-    IEnumerator ExitToMainMenuAfterDelay()
-    {
-        Debug.Log("Game Over! Exiting to Main Menu in 2 seconds");
-        yield return new WaitForSecondsRealtime(2f); // Wait for 2 seconds
-        Time.timeScale = 1; // Restore time scale before loading the scene
-        Debug.Log("Game Over! Exiting to Main Menu");
-        SceneHandler.Instance.LoadMenuScene();
     }
 }
